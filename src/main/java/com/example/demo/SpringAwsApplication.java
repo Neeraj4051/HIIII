845c720d094ab1fe01e289b4faa8a3f3acd735e0
Neeraj4051 <Neeraj3686@gmail.com>
2023-12-05 18:24:57 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringAwsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsApplication.class, args);
+		System.out.println("HII NEERAJ IS HERE");
+	}
+
+}
